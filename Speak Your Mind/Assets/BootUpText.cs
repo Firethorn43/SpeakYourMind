8b240065791f450e4dec6f955728f2b51e8d0c79
isaac-duna <54054163+isaac-duna@users.noreply.github.com>
2019-11-14 21:19:56 -0500

    Fruits Text Pickup

---
+++ Speak Your Mind/Assets/BootUpText.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BootUpText : MonoBehaviour
+{
+    public GameObject bootUpText;
+    bool showText;
+
+    void Start()
+    {
+        showText = false;
+        bootUpText.SetActive(showText);
+    }
+
+    public void OnClick()
+    {
+        showText = !showText;
+        bootUpText.SetActive(showText);
+    }
+}
